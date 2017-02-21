d7561fecd2b8c4713aaa8744e4f5e0532bd25d47
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-21 16:49:17 -0600

    finish lecture

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(express.static('public'));  //"public" is a folder
+app.use(bodyParser.urlencoded({extended: true}));
+var songList = [
+            {title: 'we did not start the phire',
+              artist: 'billy joel'
+            },
+            {title: 'ring of fire',
+              artist: 'johnny cash'
+            }
+          ];
+
+app.get("/songs", function(req, res){ //does not need a . because it is not refering to a file
+  res.send(songList);
+  });
+
+app.post("/newSong", function(req, res){
+  var newSong = req.body;
+  songList.push(newSong);
+  console.log(songList);
+  res.sendStatus(200);
+  });
+
+app.listen(3000);
--- public/client.js
+++ public/client.js
@@ -1,4 +1,35 @@
-//Use only with jQuery
+
 $(document).ready(function() {
+getAllSongs();
+function getAllSongs(){
+$.ajax({
+  type: "GET",
+  url: "/songs",
+  success: function(response){
+    console.log("response", response);
+  }
+
+});
+}
+$("#addSongButton").on("click", function(){
+ var newSongTitle = $('#title').val();
+ var newSongArtist = $('#artist').val();
+  var newSongObject = {
+    title: newSongTitle,
+    artist: newSongArtist
+
+  }
+
+  $.ajax({
+    type: 'POST',
+    url: '/newSong',
+    data: newSongObject,
+    sucess: function(response){
+      console.log(request);
+    }
+
+
+  })
+});
 
 });
